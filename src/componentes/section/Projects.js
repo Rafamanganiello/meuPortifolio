98b4c495909c72239ed07884cec65f3935d52614
Rafamanganiello <rafa_manganiello@hotmail.com>
2024-05-18 02:39:54 -0300

    adição de novos projetos

--- src/componentes/section/Projects.js
+++ src/componentes/section/Projects.js
@@ -2,6 +2,8 @@ import styles from './Projects.module.css'
 import Card from './Card'
 import landingDNC from "../../images/landingDNC.png"
 import Dncweather from "../../images/Dncweather.png"
+import portvite from "../../images/portvite.png"
+import tabuada from "../../images/tabuada.png"
 
 function Projects(){
     return(
@@ -16,7 +18,17 @@ function Projects(){
             descr= "Projeto com consumo de APIs para consulta de cep e metereologia. Front-end desenvolvido com HTML e CSS puro. Consumo de api com JavaScript"
             btntxt="Acessar repositório"
             repo="https://github.com/Rafamanganiello/DncWeather"
-            site="https://dncweatherapi.netlify.app"/>
+            site="https://dncweather-1.netlify.app"/>
+            <Card img={portvite} title="Porfólio Vite+React.js"
+            descr= "Portfólio com a utilização do vite + React.js com aplicação de hooks e outros fundamentos do front-end"
+            btntxt="Acessar repositório"
+            repo="https://github.com/Rafamanganiello/portfolioVite"
+            site="https://portvite.netlify.app"/>
+            <Card img={tabuada} title="Tabuada"
+            descr= "Tabuada aplicando logica de programação e estruturas de repetição"
+            btntxt="Acessar repositório"
+            repo="https://github.com/Rafamanganiello/Tabuada"
+            site="https://easytabuada.netlify.app"/>
             
             
         </div>
