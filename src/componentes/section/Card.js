c9a7a5054d3049760b1aba831d1b44f90ed0f061
Rafamanganiello <rafa_manganiello@hotmail.com>
2024-05-17 23:45:17 -0300

    Primeira versão do projeto

--- src/componentes/section/Card.js
+++ src/componentes/section/Card.js
@@ -1,18 +1,31 @@
 import Styles from "./Card.module.css"
-
+import {useState} from 'react'
 import Btn2 from "../elements/Btn2"
 
 function Card({img,title,descr,repo,btntxt,site}){
+
+    const [info,setInfo] = useState(false)
+
+    function infOn(){
+        setInfo(true)
+    }
+    function infOff(){
+        setInfo(false)
+    }
+
+ 
     return(
         <div className={Styles.card}>
-            <a target="_blanc" href={site}>
+            <a onMouseEnter={infOn} onMouseLeave={infOff} target="_blanc" href={site}>
                 <img src={img}></img>
             </a>
-            <section>
-                <h3>{title}</h3>
-                <p><strong>Descrição: </strong>{descr}</p>
-                <Btn2 text={btntxt} link={repo}/>
-            </section>
+            {info === true && (
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Descrição: </strong>{descr}</p>
+                    <Btn2 text={btntxt} link={repo}/>
+                </section>
+            )}
         </div>
     )
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,12 +1,47 @@
 import styles from './Presentation.module.css'
 import Btn1 from '../elements/Btn1'
 import Btn2 from '../elements/Btn2'
+import {useEffect, useState} from 'react'
 
 function Presentation(){
+
+    const[text,setText]=useState(''); //Aqui é o conteudo que será exibido ou apagado da tela
+    const rotate = ['Rafael Manganiello','Desenvolvedor Front-End']; //array com os textos
+    const [loop, setLoop]=useState(0); //qual o texto do array que será exibido (o ou 1)
+    const [toDelete,setToDelete]= useState(false);//boolean que definirá se o texto será deletado ou não
+    const period = 100; //velocidade em que o texto é escrito/apagado
+    const [delta,setDelta]=useState(100) //variação da velicodade do period (nesse caso se mantem igual)
+
+
+    useEffect(()=>{
+        let ticker = setInterval (()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+    },[text])
+
+    const toType = () =>{
+
+        let i = loop % rotate.length;
+        let fullText = rotate[i]
+        let updatedText = toDelete ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+
+        if(!toDelete && updatedText === fullText){
+        setToDelete(true);
+        setDelta(period);
+        }else if (toDelete && updatedText === ''){
+        setToDelete(false);
+        setDelta(period);
+        setLoop(loop+1);
+        }
+    }
+
     return(
         <div className={styles.presentation} id='Presentation'>
             <h4><strong>Bem-vindo ao meu Portifolio</strong></h4>
-            <h1>Olá, eu sou Rafael Manganiello!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p> Sou um entusiasta da tecnologia desde os meus 16 anos.<br/>
                 Após não me identificar na carreira de cozinheiro, percebi<br/> 
                 que minha verdadeira vocação estava na área de tecnologia.<br/>
